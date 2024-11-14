6aa80f34ae9da7e9579baf95b603b511a6095a0f
nguyendung-alibaba <nguyendung3r@gmail.com>
2024-11-14 15:12:36 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php 
+
+// Require file Common
+require_once './commons/env.php'; // Khai báo biến môi trường
+require_once './commons/function.php'; // Hàm hỗ trợ
+
+// Require toàn bộ file Controllers
+require_once './controllers/HomeController.php';
+
+// Require toàn bộ file Models
+require_once './models/Student.php';
+
+// Route
+$act = $_GET['act'] ?? '/';
+// var_dump($_GET['act']);
+// die();
+
+// Để bảo bảo tính chất chỉ gọi 1 hàm Controller để xử lý request thì mình sử dụng match
+
+match ($act) {
+    // Trang chủ
+    '/' => (new HomeController())-> home(),
+    'trangchu' => (new HomeController())-> trangChu(),
+    'List' => (new HomeController())-> List(),
+    // 'List2' => (new HomeController())-> List2(),
+};
